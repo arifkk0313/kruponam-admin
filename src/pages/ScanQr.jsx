3bce627942b386b00f31cded5e39979ec73f3f2b
arifkk0313 <arifkk0313@gmail.com>
2024-09-09 21:23:18 +0530

    update

--- src/pages/ScanQr.jsx
+++ src/pages/ScanQr.jsx
@@ -1,57 +1,10 @@
-import React, { useState } from 'react';
-// import { QrReader } from 'react-qr-reader'; // Use QrReader instead of QRReader
-import axios from 'axios';
+import React from 'react'
+import { Scanner } from '@yudiel/react-qr-scanner';
 
-const AdminPanel = () => {
-    const [scannedData, setScannedData] = useState(null);
-    const [message, setMessage] = useState('');
+function ScanQr() {
+  
+    return <Scanner onScan={(result) => console.log(result)} />;
+  
+}
 
-    const handleScan = async (data) => {
-        if (data) {
-            setScannedData(data);
-
-            try {
-                // Send the scanned data to the server to update entrance status
-                const response = await axios.post('https://arrif-api.moshimoshi.cloud//api/v2/kruponam/update-entrance', {
-                    qrCode: data,
-                });
-
-                if (response.status === 200) {
-                    setMessage('Entrance status updated successfully!');
-                } else {
-                    setMessage('Failed to update entrance status.');
-                }
-            } catch (error) {
-                setMessage('An error occurred while updating entrance status.');
-            }
-        }
-    };
-
-    const handleError = (error) => {
-        setMessage('Error scanning QR code.', error);
-    };
-
-    return (
-        <div style={{ padding: '20px', backgroundColor: '#f4f4f4', borderRadius: '8px' }}>
-            <h1>Admin Panel</h1>
-            <div style={{ marginBottom: '20px' }}>
-                {/* <QrReader
-                    onResult={(result, error) => {
-                        console.log(error,'pppp')
-                        console.log(result,"3333")
-                        if (result) {
-                            handleScan(result.text);
-                        }
-                        if (error) {
-                            handleError(error);
-                        }
-                    }}
-                    style={{ width: '100%' }}
-                /> */}
-            </div>
-            {message && <p>{message}</p>}
-        </div>
-    );
-};
-
-export default AdminPanel;
+export default ScanQr
